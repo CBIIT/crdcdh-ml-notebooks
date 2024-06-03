d76372ebfd2ef225fccc8ceda896c919bead16e6
vshand11 <105606628+vshand11@users.noreply.github.com>
2024-06-03 17:00:25 -0400

    update

--- src/common/config.py
+++ src/common/config.py
@@ -7,11 +7,11 @@ def get_current_folder(global_variables):
     # if calling from a file
     if "__file__" in global_variables:
         current_file = Path(global_variables["__file__"])
-        current_folder = current_file.parent.parent.resolve()
+        current_folder = current_file.parent.resolve()
     # if calling from a notebook
     else:
-        current_folder = Path(os.getcwd()).parent
-    return current_folder
+        current_folder = Path(os.getcwd())
+    return current_folder.parent
 
 current_folder = get_current_folder(globals())
 print("src dir:" + str(current_folder))
@@ -20,15 +20,16 @@ print("root dir:" + str(ROOT_DIR))
 DATASETS_S3_PREFIX = "datasets"
 OUTPUTS_S3_PREFIX = "outputs"
 
-SOURCE_S3_PREFIX = "ml-data"
-SOURCE_S3_BUCKET = "crdcdh-test-submission"
+# model source s3 configurations
+SOURCE_S3_PREFIX = ""
+SOURCE_S3_BUCKET = ""
 SOURCE_S3_PATH = f"s3://{SOURCE_S3_BUCKET}/{SOURCE_S3_PREFIX}"
 
-SOLUTION_PREFIX = "myTest"
+SOLUTION_PREFIX = "crdcdh-ml"
 
-TRAINING_INSTANCE_TYPE = "ml.t3.medium"
+TRAINING_INSTANCE_TYPE = "ml.c4.xlarge"
 HOSTING_INSTANCE_TYPE = "ml.t3.medium"
 
 TAG_KEY = "crdcdh-ml-dev"
-
-SAGEMAKER_EXECUTE_ROLE = "arn:aws:iam::420434175168:role/service-role/AmazonSageMaker-ExecutionRole-20240524T161395"
+# execute-role for sagemaker
+SAGEMAKER_EXECUTE_ROLE = ""
