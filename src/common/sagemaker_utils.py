3bb3e554a0f992f6280871e392089d4cc6d9db82
vshand11 <105606628+vshand11@users.noreply.github.com>
2024-06-03 18:11:43 -0400

    update

--- src/common/sagemaker_utils.py
+++ src/common/sagemaker_utils.py
@@ -1,15 +1,17 @@
 import os
+import boto3
 from sagemaker.session import Session
 from sagemaker.config import load_sagemaker_config, validate_sagemaker_config
 from sagemaker.local import LocalSession
-import common.sagemaker_config_sample as sagemaker_config_sample
+from botocore.exceptions import ClientError
+import common.sagemaker_config as sagemaker_config
 
 def get_sagemaker_session(is_Local = False):
     if not is_Local: 
         # Create a configuration dictionary manually
         custom_sagemaker_config = load_sagemaker_config(
             additional_config_paths=[
-                os.path.join(sagemaker_config_sample.ROOT_DIR, "configs/sagemaker/config.yaml")
+                os.path.join(sagemaker_config.ROOT_DIR, "configs/sagemaker/config.yaml")
             ]
         )
         # Then validate that the dictionary adheres to the configuration schema
@@ -24,3 +26,17 @@ def get_sagemaker_session(is_Local = False):
         sagemaker_session = LocalSession()
         sagemaker_session.config = {'local': {'local_code': True, 'region_name': "us-east-1" }}
         return sagemaker_session
+
+def delete_endpoint(endpoint_name):
+    sagemaker_client = None
+    try:
+        sagemaker_client = boto3.client("sagemaker")
+        sagemaker_client.delete_endpoint(EndpointName=endpoint_name)
+        sagemaker_client.delete_endpoint_config(EndpointConfigName=endpoint_name)
+    except ClientError as ce:
+        print(ce)
+    finally:
+        if not sagemaker_client:
+            sagemaker_client.close()
+            sagemaker_client = None
+    
